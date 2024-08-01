42d9c647113459b64fb587353dcfd3d837e9c039
asesdelnorte <futbolitos315@gmail.com>
2024-08-01 12:33:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
   whatsappLink.addEventListener('click', function(event) {
     event.preventDefault();
-    window.location.href = 'https://api.whatsapp.com/send/?phone=5491180319148&text=Hola%2C+estoy+interesado+en+un+usuario%2C+Mi+nombre+es:&type=phone_number&app_absent=0';
+    window.location.href = 'https://api.whatsapp.com/send/?phone=5491164626778&text=Hola%2C+estoy+interesado+en+un+usuario%2C+Mi+nombre+es:&type=phone_number&app_absent=0';
   });
 });
